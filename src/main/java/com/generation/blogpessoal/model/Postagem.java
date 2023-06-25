5f078cb75ce5ba6e13cd14ecfd46ed900213adf4
Patricia Silva <agarotadaecharpe_verde@hotmail.com>
2023-06-24 22:47:24 -0300

    Back-End 23_06

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,80 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+
+	@Id // mostrar que ela é a chave primaria da tabela que será gerado no banco de
+		// dados
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // aqui cria o auto_incremento (estratégia) do id da tabela
+	private Long id;
+
+	@NotBlank(message = "O atributo titulo é Obrigatório!") // funciona como o Not Null - noblank não aceita nem nulo e
+															// nem vazio; o message é comentário
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres") // aqui
+																													// determinamos
+																													// o
+																													// tamanho
+																													// da
+																													// String
+																													// -
+																													// tamanho
+																													// minimo
+																													// e
+																													// maximo
+																													// (estratégia)
+	private String titulo;
+
+	@NotBlank(message = "O atributo texto é Obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no minimo 10 e no máximo 1000 caracteres")
+	private String texto;
+
+	@UpdateTimestamp // anotação + configuração da variavel = será preenchida automático com o fuso
+						// horário do sistema (data e hora)
+	private LocalDateTime data;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+}
