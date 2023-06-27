2d185d735e655bd5283350084bf3007678a27be5
Patricia Silva <agarotadaecharpe_verde@hotmail.com>
2023-06-27 18:30:01 -0300

    Atualização Classe Tema 27_06

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -4,10 +4,13 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -23,29 +26,29 @@ public class Postagem {
 
 	@NotBlank(message = "O atributo titulo é Obrigatório!") // funciona como o Not Null - noblank não aceita nem nulo e
 															// nem vazio; o message é comentário
-	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres") // aqui
-																													// determinamos
-																													// o
-																													// tamanho
-																													// da
-																													// String
-																													// -
-																													// tamanho
-																													// minimo
-																													// e
-																													// maximo
-																													// (estratégia)
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres") 
 	private String titulo;
 
 	@NotBlank(message = "O atributo texto é Obrigatório!")
 	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no minimo 10 e no máximo 1000 caracteres")
 	private String texto;
 
-	@UpdateTimestamp // anotação + configuração da variavel = será preenchida automático com o fuso
-						// horário do sistema (data e hora)
+	@UpdateTimestamp // anotação + configuração da variavel = será preenchida automático com o fuso - horário do sistema (data e hora)
 	private LocalDateTime data;
 
-	public Long getId() {
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem") //ignorar o looping na busca da postagem em relação aquele tema da busca
+	private Tema tema;
+	
+		public Long getId() {
 		return id;
 	}
 
